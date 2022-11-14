7fdbb5bb8bbc0b41fe5962591dd764bc23e5303d
jaysnel <jaylan210@gmail.com>
2022-11-14 15:53:53 -0500

    Updating contract and scripts

---
+++ contracts/Domains.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity >=0.8.0 <=0.8.20;
+
+import "hardhat/console.sol";
+
+contract Domains {
+    constructor() {
+        console.log('Creating Domains Contract');
+    }
+}
