327bbaf9d972db35eed3c530acde5852624c1d8c
jaysnel <jaylan210@gmail.com>
2022-11-18 14:27:43 -0500

    Registering address with data and returning that data as a view

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -7,4 +7,29 @@ contract Domains {
     constructor() {
         console.log('Creating Domains Contract');
     }
+
+    mapping(string => address) public domains;
+    mapping(string => string) public records;
+
+    function register(string calldata name) public {
+        // Check that the name is unregistered
+        require(domains[name] == address(0));
+
+        domains[name] = msg.sender;
+        console.log('%s has registered a domain!', msg.sender);
+    }
+
+    function getAddress(string calldata name) public view returns (address) {
+        return domains[name];
+    }
+
+    function setRecord(string calldata name, string calldata record) public {
+        // Check that the owner is the transaction sender
+        require(domains[name] == msg.sender);
+        records[name] = record;
+    }
+
+    function getRecord(string calldata name) public view returns (string memory) {
+        return records[name];
+    }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,24 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains')
     const domainContract = await domainContractFactory.deploy();
     await domainContract.deployed();
     console.log('Contract deployed at: ', domainContract.address);
+    console.log('Contract deployed by: ', owner.address);
+
+    const txn = await domainContract.register('DOOM');
+    await txn.wait();
+
+    const domainOwner = await domainContract.getAddress('DOOM');
+    console.log('Owner of domain: ', domainOwner);
+
+    // Trying to set a record that doesn't belong to me!
+    try {
+        txn = await domainContract.connect(randomPerson).setRecord('DOOM', "Haha my domain now!");
+        await txn.wait();
+    } catch (err) {
+        console.log('You dont own this domain.');
+    }
 }
 
 const runMain = async () => {
