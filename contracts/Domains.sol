93a5510369ec48df7d906fb8770b7bef5b48ac92
jaysnel <jaylan210@gmail.com>
2022-11-28 12:45:20 -0500

    creating ENS and starting to also make it a NFT

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -1,19 +1,43 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity >=0.8.0 <=0.8.20;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+
+import { StringUtils } from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains {
-    constructor() {
-        console.log('Creating Domains Contract');
-    }
+    string public tld;
 
     mapping(string => address) public domains;
     mapping(string => string) public records;
 
-    function register(string calldata name) public {
+    constructor(string memory _tld) payable {
+        tld = _tld;
+        console.log('Creating Domains Contract');
+    }
+    
+    function price(string calldata name) public pure returns(uint) {
+        uint len = StringUtils.strlen(name);
+        require(len > 0);
+        if (len == 3) {
+                return 5 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
+            } else if (len == 4) {
+                return 3 * 10**17; // To charge smaller amounts, reduce the decimals. This is 0.3
+            } else {
+                return 1 * 10**17;
+            }
+
+    }
+
+    function register(string calldata name) public payable {
         // Check that the name is unregistered
         require(domains[name] == address(0));
+
+        uint _price = price(name);
+        require(msg.value >= _price, "Not enough Matic paid");
 
         domains[name] = msg.sender;
         console.log('%s has registered a domain!', msg.sender);
--- scripts/run.js
+++ scripts/run.js
@@ -1,24 +1,27 @@
 const main = async () => {
     const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains')
-    const domainContract = await domainContractFactory.deploy();
+    const domainContract = await domainContractFactory.deploy('naruto');
+    const domainName = '9tails';
     await domainContract.deployed();
     console.log('Contract deployed at: ', domainContract.address);
-    console.log('Contract deployed by: ', owner.address);
 
-    const txn = await domainContract.register('DOOM');
+    const txn = await domainContract.register(domainName, {value: hre.ethers.utils.parseEther('0.1')});
     await txn.wait();
 
-    const domainOwner = await domainContract.getAddress('DOOM');
-    console.log('Owner of domain: ', domainOwner);
+    const address = await domainContract.getAddress(domainName);
+    console.log(`Owner of ${domainName}: `, address);
+
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log('Contract balance: ', hre.ethers.utils.formatEther(balance));
 
     // Trying to set a record that doesn't belong to me!
-    try {
-        txn = await domainContract.connect(randomPerson).setRecord('DOOM', "Haha my domain now!");
-        await txn.wait();
-    } catch (err) {
-        console.log('You dont own this domain.');
-    }
+    // try {
+    //     txn = await domainContract.connect(randomPerson).setRecord('DOOM', "Haha my domain now!");
+    //     await txn.wait();
+    // } catch (err) {
+    //     console.log('You dont own this domain.');
+    // }
 }
 
 const runMain = async () => {
